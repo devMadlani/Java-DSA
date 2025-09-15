f1844c8b3438375bb2f42b9d300b5fa3401fa52a
Dev Madlani <madlanidev@gmail.com>
2025-09-15 06:47:07 +0530

    solved leetcode question

---
+++ 05_linear-search-DSA/src/MaxCustomerWealth.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/richest-customer-wealth/description/
+
+public class MaxCustomerWealth {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,5},{3,2,1}};
+        int RichestWealth = maximumWealth(arr);
+        System.out.println(RichestWealth);
+    }
+    static int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for(int[] rows : accounts){
+            int sum = 0;
+            for(int account: rows){
+                sum+=account;
+            }
+            if(sum > maxWealth) {
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
