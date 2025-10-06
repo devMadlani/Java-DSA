bfcc7b95a53cffbc265566dafb8f0985c00bc974
Dev Madlani <madlanidev@gmail.com>
2025-10-06 22:43:02 +0530

    Ceiling and floor of a number

---
+++ 06_Binary-search-DSA/src/FloorOfNum.java
@@ -0,0 +1,41 @@
+public class FloorOfNum {
+
+    public static void main(String[] args) {
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {89,46,24,6,3,0,-1};
+        int target = 14;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(isAsc){
+                if(target > arr[mid]) {
+                    start = mid + 1;
+                } else if(target < arr[mid]){
+                    end = mid - 1;
+                }
+            } else {
+                if(target < arr[mid]) {
+                    start = mid + 1;
+                } else if(target > arr[mid]){
+                    end = mid - 1;
+                }
+            }
+        }
+        return  isAsc ? end : start;
+    }
+
+}
