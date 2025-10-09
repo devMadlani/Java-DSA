4425275be0bcfd7a283e471054adb4265acdf85a
Dev Madlani <madlanidev@gmail.com>
2025-10-09 07:42:51 +0530

    leet code 33 search in rotated Array

---
+++ 06_Binary-search-DSA/src/leetcode/Q_33_SearchInRotatedArr.java
@@ -0,0 +1,72 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+package leetcode;
+
+public class Q_33_SearchInRotatedArr {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 6, 7, 1, 2};
+        int target = 7;
+        int ans = search(nums, target);
+        System.out.println(ans);
+    }
+
+    static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+//        my approch
+//        int firstTry = binarySearch(nums, target, start, pivot - 1);
+//        if (firstTry != -1) {
+//            return firstTry;
+//        }
+//        return binarySearch(nums, target, pivot + 1, end);
+
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    static int binarySearch(int[] nums, int target, int start, int end) {
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+            if (nums[start] > nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
